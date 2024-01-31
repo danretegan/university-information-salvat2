40636dece5dfc8d65cf764c7480ca60e4195c89e
Dan <danretegan@yahoo.com>
2024-01-31 08:55:20 +0200

    Yes! Primul meu hook!

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import { Component } from "react";
 import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Tutors from "./components/tutors/Tutors";
 import University from "./components/university/University";
 import Cities from "./components/cities/cities";
 
-class App extends Component {
-  render() {
-    return (
-      <main className="App">
-        <Sidebar />
-        <section className="container">
-          <University />
-          <Tutors />
-          <Cities />
-        </section>
-      </main>
-    );
-  }
-}
+const App = () => {
+  console.log("Yes! Primul meu hook!");
+  return (
+    <main className="App">
+      <Sidebar />
+      <section className="container">
+        <University />
+        <Tutors />
+        <Cities />
+      </section>
+    </main>
+  );
+};
 
 export default App;
