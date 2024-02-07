46a0d52a4180fee0dacce8823fea0c5e01d3c08f
Dan <danretegan@yahoo.com>
2024-02-07 18:27:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Tutors from "./components/tutors/Tutors";
 import University from "./components/university/University";
-import Cities from "./components/cities/cities";
+import Cities from "./components/cities/Cities";
 
 const App = () => {
   console.log("Yes! Primul meu hook!");
