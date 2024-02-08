b0a2e0a6a3d9dcbd9a91cd8d0edf6b8baa414762
Dan <danretegan@yahoo.com>
2024-02-08 12:32:53 +0200

    NotFoundPage setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import SharedLayout from "./pages/SharedLayout";
 import HomePage from "./pages/HomePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 const App = () => {
   console.log("Yes! Primul meu hook!");
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/university-information" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </>
