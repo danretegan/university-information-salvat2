89b3696b1433dffa2c002415b5e18f76488a7fa3
Dan <danretegan@yahoo.com>
2024-02-08 13:00:09 +0200

    Sidebar update

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import styles from "./Sidebar.module.css";
 import { HiAcademicCap, HiBookOpen } from "react-icons/hi";
 
@@ -7,11 +8,13 @@ const Sidebar = () => {
       id: "1",
       name: "University",
       icon: <HiBookOpen />,
+      path: "/university-information",
     },
     {
       id: "2",
       name: "Faculties",
       icon: <HiAcademicCap />,
+      path: "/university-information/faculties",
     },
   ];
 
@@ -21,7 +24,15 @@ const Sidebar = () => {
       <ul>
         {menuItems.map((item) => (
           <li key={item.id}>
-            {item.icon} {item.name}
+            <NavLink
+              key={item.id}
+              to={item.path}
+              className={({ isActive }) =>
+                [styles.navLink, isActive ? styles.navLinkActive : ""].join("")
+              }
+            >
+              {item.icon} {item.name}
+            </NavLink>
           </li>
         ))}
       </ul>
