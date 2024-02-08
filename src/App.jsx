5c8295f5169106b506594b78296a28f68697ffa9
Dan <danretegan@yahoo.com>
2024-02-08 12:24:13 +0200

    add SharedLayout & HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import "./App.css";
-import Sidebar from "./components/sidebar/Sidebar";
-import Tutors from "./components/tutors/Tutors";
-import University from "./components/university/University";
-import Cities from "./components/cities/Cities";
+import { Route, Routes } from "react-router-dom";
+import SharedLayout from "./pages/SharedLayout";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   console.log("Yes! Primul meu hook!");
   return (
-    <main className="App">
-      <Sidebar />
-      <section className="container">
-        <University />
-        <Tutors />
-        <Cities />
-      </section>
-    </main>
+    <>
+      <Routes>
+        <Route path="/university-information" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import University from "../components/university/University";
+import Tutors from "../components/tutors/Tutors";
+import Cities from "../components/cities/Cities";
+
+function HomePage() {
+  return (
+    <>
+      <University />
+      <Tutors />
+      <Cities />
+    </>
+  );
+}
+
+export default HomePage;
