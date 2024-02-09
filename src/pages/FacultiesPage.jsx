1928b72f1c5edf7fb36e709edf1bf3799bb3cf79
Dan <danretegan@yahoo.com>
2024-02-09 11:57:03 +0200

    Update FacultiesPage.jsx

--- src/pages/FacultiesPage.jsx
+++ src/pages/FacultiesPage.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import citiesService from "../services/citiesService";
+import Error from "../components/common/error/Error";
 
 const FacultiesPage = () => {
   const [list, setList] = useState([]);
@@ -6,16 +9,42 @@ const FacultiesPage = () => {
   const [error, setError] = useState("");
 
   useEffect(() => {
-    // get faculties data
-  });
+    async function getCities() {
+      const response = await citiesService.getCities();
+      setList(response);
+
+      return response;
+    }
+    setIsLoading(true);
+    getCities()
+      .catch((error) => {
+        console.error(error);
+        setError("A aparut o eroare la obtinerea listei de orase");
+      })
+      .finally(setIsLoading(false));
+  }, []);
 
   if (isLoading) {
     return <div>Se incarca...</div>;
   }
 
+  if (error && error.length > 0) {
+    return <Error message={error} />;
+  }
+
   return (
     <div>
       <h2>Faculties</h2>
+      <div>
+        {list.map((item) => (
+          <div key={item.id}>
+            {item.name}
+            <Link to={`/faculties/${item.id}/description`}>
+              <span>Details</span>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
