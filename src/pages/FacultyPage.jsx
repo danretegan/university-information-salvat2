68c8f67bb3e79e001c0cf06711647c8b45670619
Dan <danretegan@yahoo.com>
2024-02-09 12:10:57 +0200

    Update FacultyPage.jsx

--- src/pages/FacultyPage.jsx
+++ src/pages/FacultyPage.jsx
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 const FacultyPage = () => {
-  return <div>Faculty Page</div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default FacultyPage;
