193bc76d299909ffcd2233b5059d39a882bdcc20
Dan <danretegan@yahoo.com>
2024-02-23 09:44:41 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,19 @@ import { createStore } from "redux";
 import { devToolsEnhancer } from "@redux-devtools/extension";
 
 const rootReducer = (state, action) => {
-  return state;
+    switch (action.type) {
+      // In functie de tipul actiunii, se va executa o logica diferita:
+      case "faculties/setSearchTerm":
+        return {
+          ...state,
+          faculties: {
+            ...state.faculties,
+            serchTerm: action.payload,
+          },
+        };
+      default:
+        return state;
+    }
 };
 
 const enhancer = devToolsEnhancer();
