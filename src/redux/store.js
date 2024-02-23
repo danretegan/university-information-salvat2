8b363e332015e093bb6b5bb734a8a46efcd99fb6
Dan <danretegan@yahoo.com>
2024-02-23 17:33:20 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,26 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 import { devToolsEnhancer } from "@redux-devtools/extension";
+import { facultiesReducer } from "./reducers/facultiesReducer";
 
-const rootReducer = (state, action) => {
-    switch (action.type) {
-      // In functie de tipul actiunii, se va executa o logica diferita:
-      case "faculties/setSearchTerm":
-        return {
-          ...state,
-          faculties: {
-            ...state.faculties,
-            serchTerm: action.payload,
-          },
-        };
-      default:
-        return state;
-    }
-};
+export const rootReducer = combineReducers({
+  faculties: facultiesReducer,
+});
+
+// const rootReducer = (state, action) => {
+//     switch (action.type) {
+//       // In functie de tipul actiunii, se va executa o logica diferita:
+//       case "faculties/setSearchTerm":
+//         return {
+//           ...state,
+//           faculties: {
+//             ...state.faculties,
+//             serchTerm: action.payload,
+//           },
+//         };
+//       default:
+//         return state;
+//     }
+// };
 
 const enhancer = devToolsEnhancer();
 export const store = createStore(rootReducer, enhancer);
