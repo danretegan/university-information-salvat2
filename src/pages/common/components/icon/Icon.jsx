59f33660f6c71de34affaa9165ba6efcab5af82a
Dan <danretegan@yahoo.com>
2024-02-24 08:10:08 +0200

    Update Icon.jsx

--- src/pages/common/components/icon/Icon.jsx
+++ src/pages/common/components/icon/Icon.jsx
@@ -1,11 +1,10 @@
-import PropTypes from "prop-types";
 import robotHead from "../../../../images/icons/robot-head.png";
 import cat from "../../../../images/icons/cat.png";
 import pin from "../../../../images/icons/pin.png";
 import pencil from "../../../../images/icons/pencil.png";
 import handPointing from "../../../../images/icons/hand-pointing.png";
 
-function Icon({ variant, label, size = 24 }) {
+export default function Icon({ variant, label, size = 24 }) {
   function getVariant(variant) {
     switch (variant) {
       case "robot":
@@ -19,18 +18,9 @@ function Icon({ variant, label, size = 24 }) {
       case "handpointing":
         return handPointing;
       default:
-        return pin; // Înlocuit cu o valoare implicită sau poți returna null sau o altă imagine implicită
+        return <robot />;
     }
   }
 
   return <img src={getVariant(variant)} alt={label} height={size} />;
 }
-
-Icon.propTypes = {
-  variant: PropTypes.oneOf(["robot", "cat", "pin", "pencil", "handpointing"])
-    .isRequired,
-  label: PropTypes.string.isRequired,
-  size: PropTypes.number,
-};
-
-export default Icon;
