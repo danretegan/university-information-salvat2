bee3ad9478785d3970002649ea1666c8178f1978
Dan <danretegan@yahoo.com>
2024-02-28 09:15:35 +0200

    Update tutorsService.js

--- src/pages/common/services/tutorsService.js
+++ src/pages/common/services/tutorsService.js
@@ -4,8 +4,13 @@ async function getTutors() {
   return apiRequest.fetchData("/tutors/");
 }
 
+async function createTutor(tutor) {
+  return apiRequest.createData("/tutors/", tutor);
+}
+
 const tutorsService = {
   getTutors,
+  createTutor,
 };
 
 export default tutorsService;
