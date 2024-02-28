b414dda08f8f7ae96c7d20a932ef44b7ba4501a9
Dan <danretegan@yahoo.com>
2024-02-28 13:02:52 +0200

    Update Tutors.jsx

--- src/pages/universities/components/tutors/Tutors.jsx
+++ src/pages/universities/components/tutors/Tutors.jsx
@@ -1,46 +1,93 @@
 import styles from "./Tutors.module.css";
-import Button from "../../../common/components/button/Button";
 import Icon from "../../../common/components/icon/Icon";
+import Error from "../../../common/components/error/Error";
+import Button from "../../../common/components/button/Button";
+import Loading from "../../../common/components/loading/Loading";
+import SearchBar from "../../../common/components/searchBar/SearchBar";
 import AddTutor from "./addTutor/AddTutor";
 import { useEffect, useState } from "react";
-import tutorsService from "../../../common/services/tutorsService";
+import { useDispatch, useSelector } from "react-redux";
+import { addTutor, fetchTutors } from "../../../../redux/slices/tutorsSlice";
 
-const TUTORS_KEY = "tutors";
-
-const Tutors = () => {
+export default function Tutors() {
+  const [searchTerm, setSearchTerm] = useState("");
   const [isAddFormVisible, setIsAddFormVisible] = useState(false);
-  const [list, setList] = useState([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [addRequestStatus, setAddRequestStatus] = useState("idle");
+  const dispatch = useDispatch();
+  const list = useSelector((state) => state.tutors.items);
+  const error = useSelector((state) => state.tutors.error);
+  const tutorsStatus = useSelector((state) => state.tutors.status);
+  const isLoading = tutorsStatus === "isloading";
 
+  // GET
   useEffect(() => {
-    async function getTutors() {
-      try {
-        setIsLoading(true);
-        const response = await tutorsService.getTutors();
-        setList(response);
-      } catch (error) {
-        setError("Error fetching tutors");
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
+    if (tutorsStatus === "idle") {
+      dispatch(fetchTutors());
     }
+  }, [tutorsStatus, dispatch]);
 
-    getTutors();
-  }, []);
+  return (
+    <section className="section">
+      <h2>
+        <Icon variant="cat" label="Tutors" />
+        <span>Tutors</span>
+      </h2>
+      {error.length > 0 && <Error message={error} />}
+      {!error && renderTutors()}
+    </section>
+  );
 
-  useEffect(() => {
-    localStorage.setItem(TUTORS_KEY, JSON.stringify(list));
-  }, [list]);
+  function renderTutors() {
+    const filteredList =
+      searchTerm.length > 0
+        ? list.filter((el) => el.firstName.includes(searchTerm))
+        : list;
 
-  const renderList = (items) => {
-    if (isLoading) {
+    return (
+      <>
+        <div className={`box ${styles.tutorsList}`}>
+          {renderList(filteredList)}
+        </div>
+
+        {isAddFormVisible && <AddTutor onFormSubmit={handleAddTutor} />}
+        {isLoading && <Loading />}
+
+        <SearchBar
+          handleChange={(evt) => {
+            setSearchTerm(evt.target.value);
+          }}
+          placeholder="Search for tutor..."
+          searchTerm={searchTerm}
+        />
+
+        <div className={"mt-16"}>
+          <Button
+            action={() => {
+              setIsAddFormVisible(true);
+            }}
+            isDisabled={addRequestStatus === "pending"}
+          >
+            {addRequestStatus === "pending" ? "Adding tutor..." : "Add tutor"}
+          </Button>
+        </div>
+      </>
+    );
+  }
+
+  // Render the list of tutors
+  function renderList(items) {
+    if (!items || !Array.isArray(items)) {
       return <>Loading...</>;
     }
 
-    if (error) {
-      return <div>Error: {error}</div>;
+    if (items.length === 0) {
+      const hasSearchTerm = searchTerm.length > 0;
+
+      return hasSearchTerm ? (
+        <div>No tutors matching with this name have been found.</div>
+      ) : (
+        <div>There are no tutors.</div>
+      );
     }
 
     return items.map((el) => {
@@ -52,50 +99,32 @@ const Tutors = () => {
           <div className={styles.address}>
             <span>{el.email}</span>
             <span>{el.telephone}</span>
-            <span>{el.city}</span>
+            <span>{el.location}</span>
           </div>
           <div>{el.role}</div>
         </div>
       );
     });
-  };
+  }
 
-  const handleTutor = (date) => {
-    const newId = list.length > 0 ? list.length : 0;
+  async function handleAddTutor(data) {
+    try {
+      setAddRequestStatus("pending");
 
-    const tutorToAdd = {
-      id: newId,
-      firstName: date.name,
-      lastName: date.surname,
-      telephone: date.phone,
-      email: date.email,
-      city: date.city,
-      role: "member",
-    };
+      const tutorToAdd = {
+        firstName: data.name,
+        lastName: data.surname,
+        telephone: data.phone,
+        email: data.email,
+        city: data.city,
+      };
 
-    /**
-     * Pentru orice state care este un obiect sau array si avem nevoie de starea precedenta, folosim metoda de mai jos:
-     */
-    setList((prevState) => {
-      return [...prevState, tutorToAdd];
-    });
-
-    setIsAddFormVisible(false);
-  };
-
-  return (
-    <section className="section">
-      <h1>
-        <Icon variant="cat" label="cat" />
-        <span>Tutors</span>
-      </h1>
-      {isLoading && <div>Loading...</div>}
-      {error && <div>Error: {error}</div>}
-      <div className={`box ${styles.tutorsList}`}>{renderList(list)}</div>
-      {isAddFormVisible && <AddTutor onFormSubmit={handleTutor} />}
-      <Button action={() => setIsAddFormVisible(true)}>Add Tutor</Button>
-    </section>
-  );
-};
-
-export default Tutors;
+      await dispatch(addTutor(tutorToAdd));
+    } catch (err) {
+      console.error("Failed to save the post: ", err);
+    } finally {
+      setIsAddFormVisible(false);
+      setAddRequestStatus("idle");
+    }
+  }
+}
